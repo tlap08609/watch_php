2eb70b8698acd4fe6c9ec3de6202148a618d0267
tlap08609 <tlap086091@livemail.tw>
2016-05-20 01:17:29 +0800

    add mysqli

--- adm/aIndex.php
+++ adm/aIndex.php
@@ -7,6 +7,22 @@
 				<h1>管理者，你好！</h1>
 				<a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle
 					Menu</a>
+
+<?php
+include ('../specSql/db_connection.php');
+$sql = "SELECT uid,email FROM usr";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>uid</th><th>email</th></tr>";
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["uid"]."</td><td>".$row["email"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+?>
 			</div>
 		</div>
 	</div>
