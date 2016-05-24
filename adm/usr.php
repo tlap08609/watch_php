c97a7533f8e563357f0dc467269f14f18830be98
tlap08609 <tlap086091@livemail.tw>
2016-05-24 16:44:00 +0800

    table+action

--- adm/usr.php
+++ adm/usr.php
@@ -3,15 +3,24 @@
 if (isset($_POST['u'])) {
 //TODO 編輯
 }
+if (isset($_POST['s'])) {
+	//TODO 編輯
+	echo $_POST['s'];
+}
 if (isset($_POST['d'])) {
 	include ('../specSql/db_connection.php');
 	$stmt = $conn->prepare("UPDATE usr SET status=CASE WHEN status=0 THEN 1 ELSE 0 END WHERE uid=?");
 	$stmt->bind_param("s", $_POST['d']);
 	$stmt->execute();
 }
+//query
+if (isset($_POST['e'])) {
+	echo $_POST['e'];
+}
 $sql = "SELECT uid,name,email,tel,mobile,address,cr,up,status
-         FROM usr";
-$where = " WHERE status=1";
+         FROM usr
+		WHERE status=1";
+
 
 
 ?>
@@ -21,9 +30,10 @@ $where = " WHERE status=1";
 		<div class="row"><div class="col-md-12">
 			<form action="usr.php" method="post">
 				<div class="panel panel-info"><div class=panel-heading><h3 class=panel-title>會員管理</h3></div><div class=panel-body>
-					<div class=col-sm-4><div class=form-group><label for=f>帳號</label><input class=form-control id=f></div></div>
-					<div class=col-sm-4><div class=form-group><label for=f>信箱</label><input class=form-control id=f></div></div>
-					<div class=col-sm-4><div class=form-group><label for="from">註冊時間:從</label><input class=form-control type="text" id="from" name="from">
+					<div class=col-sm-3><div class=form-group><label for=s>會員狀態</label><select class=form-control id="s" name="s"><option>上線會員</option><option>禁用會員</option></select></div></div>
+					<div class=col-sm-3><div class=form-group><label for=i>帳號</label><input class=form-control id=i></div></div>
+					<div class=col-sm-3><div class=form-group><label for=e>信箱</label><input class=form-control id=e name=e></div></div>
+					<div class=col-sm-3><div class=form-group><label for="from">註冊時間:從</label><input class=form-control type="text" id="from" name="from">
 					<label for="to">註冊時間:到</label><input class=form-control type="text" id="to" name="to"></div></div>
 				</div>
 				<div class=panel-footer>
@@ -37,8 +47,9 @@ $where = " WHERE status=1";
 					while($row = $result->fetch_assoc()) {?>
 					<tr><td><?php echo $row["name"]?></td><td><?php echo $row["email"]?></td><td><?php echo empty($row["tel"])?"無":$row["tel"]?></td><td><?php echo empty($row["mobile"])?"無":$row["mobile"]?></td><td><?php echo empty($row["address"])?"無":$row["address"]?></td><td><?php echo $row["cr"]?></td><td><?php echo $row["up"]?></td><td><?php echo ($row["status"]==1)?"上線會員":"禁用會員"?></td>
 					<td><div class=btn-group role=group>
-					<button class="btn btn-primary" name="u" value="<?php echo $row['uid']?>">編輯</button>
-					<button class="btn btn-danger" name="d" value="<?php echo $row['uid']?>">停用</button></div></td></tr><?php 
+					<button class="btn btn-primary" name="u" value="<?php echo $row['uid']?>">查看</button>
+					<?php if ($row["status"]==1) {?><button class="btn btn-danger" name="d" value="<?php echo $row['uid']?>">停用</button><?php } else{?><button class="btn btn-info" name="d" value="<?php echo $row['uid']?>">啟用</button><?php }?></div></td></tr>
+					<?php 
  					}?>
  				</table><?php 
  			} else {?>
@@ -96,30 +107,15 @@ $where = " WHERE status=1";
         $( "#from" ).datepicker( "option", "maxDate", selectedDate );
       }
     });
-    $( "#oUid" ).button({
+    $( "#oUid,#oEmail,#oCr,#oUp" ).button({
         icons: {
       	primary: "ui-icon-triangle-1-s",
         },
         text: false
       })
-    $( "#oEmail" ).button({
-        icons: {
-      	primary: "ui-icon-triangle-1-s",
-        },
-        text: false
-      })
-    $( "#oCr" ).button({
-        icons: {
-      	primary: "ui-icon-triangle-1-s",
-        },
-        text: false
-      })
-    $( "#oUp" ).button({
-        icons: {
-      	primary: "ui-icon-triangle-1-s",
-        },
-        text: false
-      })
+    $('#s').change(function() { 
+        $(this).parents('form').submit(); 
+     });
 
   });
  </script>
--- specSql/_usr.sql
+++ specSql/_usr.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS `usr`(
 `name` varchar(50) NOT NULL COMMENT '/[a-z\u4e00-\u9eff]{1,}/i',  
 `pw` varchar(16) NOT NULL COMMENT 'md5',
 `email` varchar(255) NOT NULL COMMENT '/[\w\d]+@[\w]{1,}.[\w]{2,3}/',
+`gender` tinyint(1) DEFAULT NULL COMMENT '0 female, 1 male',
 `tel` varchar(20) DEFAULT NULL,
 `mobile` varchar(10) DEFAULT NULL COMMENT '/09[0-9]{8}|09[0-9]{2}-[0-9]{6}/',
 `address` varchar(255) NULL,
@@ -15,4 +16,7 @@ UNIQUE KEY `email` (`email`)
 
 INSERT INTO `usr`(`uid`,`name`,`pw`,`email`,`cr`) VALUES
 ('7441005c-1cee-11e6-adf4-b888e3065a51','tlap08609','cool0519','tlap086091@livemail.tw',now()),
-('c59c5aa1-1cef-11e6-adf4-b888e3065a51','alice','1300197','alice@livemail.tw',now())
+('c59c5aa1-1cef-11e6-adf4-b888e3065a51','alice','1300197','alice@livemail.tw',now()),
+(uuid(),'eva','eva197','eva@livemail.tw',now()),
+(uuid(),'mom','mom197','mom@livemail.tw',now()),
+(uuid(),'father','fat197','father@livemail.tw',now())
